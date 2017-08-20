67da456dd958f0a1ca4adf3d5d9d2677cd3a3bdf
Mauro DeLazeri <mauro.delazeri@isprime.com>
2017-08-19 20:13:48 -0400

    ajusts

--- gorilla.go
+++ gorilla.go
@@ -28,14 +28,8 @@ var (
 )
 
 func init() {
-	// Log as JSON instead of the default ASCII formatter.
 	log.SetFormatter(&log.JSONFormatter{})
-
-	// Output to stdout instead of the default stderr
-	// Can be any io.Writer
 	log.SetOutput(os.Stdout)
-
-	// Only log the warning severity or above.
 	log.SetLevel(log.WarnLevel)
 }
 
